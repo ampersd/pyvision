6d25eb6e600e00201901708ad8d69b06e60712e1
Anton Korepanov <tony1000@ya.ru>
2016-04-14 23:06:51 +0500

    update ipython-notebooks examples

---
+++ samples/WACV2012_Tutorial/ipython notebooks/test.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[2]:
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+img = mpimg.imread('stinkbug.png')
+plt.imshow(img)
+
+# In[ ]:
+
+
+
